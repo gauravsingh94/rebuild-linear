3d276d086b6022500db86b54f027ae9d37f30c58
Gaurav Singh <99260988+gauravsingh94@users.noreply.github.com>
2024-12-30 01:16:27 +0530

    added the footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import Footer from "@/app/components/Footer";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import "./globals.css";
 
